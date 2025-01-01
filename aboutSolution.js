0e4f3b56690289ed6de5414a187902ae11fa2353
BugHunter-X <askzenai@gmail.com>
2025-01-01 14:18:07 +0100

    Add aboutSolution.js file

---
+++ aboutSolution.js
@@ -0,0 +1,14 @@
+```javascript
+// pages/about.js
+function About() {
+  console.log('About page is rendering'); // Added console log for debugging
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
+export default About;
+```
